f6c8e24ebe5a4fc83f1ba078ec0cda0045444683
Petri Alapiessa <palapiessa@gmail.com>
2015-08-11 09:18:54 +0300

    Collect input parameters from command line

--- replaceStringInFile/Program.cs
+++ replaceStringInFile/Program.cs
@@ -7,63 +7,73 @@ using System.Threading.Tasks;
 
 namespace replaceStringInFile
 {
+    public class Input {
+        public string searchFor {get;set;}
+        public string replaceWith { get; set; }
+
+        public Input(string searchstring, string replacestring)
+        {
+            searchFor = searchstring;
+            replaceWith = replacestring;
+        }
+    }
     class Program
     {
-        
         static void Main(string[] args)
         {
             //
             // replaceStrinInFile.exe
-            // Input parameter: root folder
+            // Input parameters: search string, replacestring, root folder
             // Description: replaces string in all wanted files. 
-            // Filenames to change can be specified settings. Root folder for
-            // filesearches must be given at command promt.
-            // String to be replaced and new strings must be given in project settings.
+            // Filenames to change can be specified settings. 
             //
-            foreach (string path in args)
+            string m1 = "\nType a string to be replaced then press Enter. "; 
+            string m2 = "\nType a string to replace with then press Enter. ";
+            string m3 = "\nType root path for starting file searches for filenames defined in settings. ";
+            Console.WriteLine(m1);
+            string searchFor = Console.ReadLine();
+            Console.WriteLine(m2);
+            string replaceWith = Console.ReadLine();
+            Console.WriteLine(m3);
+            string path = Console.ReadLine();
+
+            Input replaceInput = new Input(searchFor, replaceWith);
+
+            if (Directory.Exists(path))
             {
-                if (File.Exists(path))
-                {
-                    // This path is a file
-                    ProcessFile(path);
-                }
-                else if (Directory.Exists(path))
-                {
-                    // This path is a directory
-                    ProcessDirectory(path);
-                }
-                else
-                {
-                    Console.WriteLine("{0} is not a valid file or directory.", path);
-                }
-            }        
-            
+                // This path is a directory
+                ProcessDirectory(path, replaceInput);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid directory.", path);
+            }    
         }
         // Process all files in the directory passed in, recurse on any directories  
         // that are found, and process the files they contain. 
-        public static void ProcessDirectory(string targetDirectory)
+        public static void ProcessDirectory(string targetDirectory, Input replaceInput)
         {
             // Process the list of files found in the directory. 
             string[] fileEntries = Directory.GetFiles(targetDirectory);
             foreach (string fileName in fileEntries)
-                ProcessFile(fileName);
+                ProcessFile(fileName, replaceInput);
 
             // Recurse into subdirectories of this directory. 
             string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
             foreach (string subdirectory in subdirectoryEntries)
-                ProcessDirectory(subdirectory);
+                ProcessDirectory(subdirectory, replaceInput);
         }
-        public static void ProcessFile(string filepath)
+        public static void ProcessFile(string path, Input input)
         {
             String searchfilename = Properties.Settings.Default.searchfilename;
 
-            if (filepath.Contains(searchfilename))
+            if (path.Contains(searchfilename))
             {
-                String search = Properties.Settings.Default.searchstring;
-                String newvalue = Properties.Settings.Default.replacestring;
+                String search =  input.searchFor;
+                String newvalue = input.replaceWith;
 
-                File.WriteAllText(filepath, File.ReadAllText(filepath).Replace(search, newvalue));
-                //Console.WriteLine("Processed file '{0}'.", path);
+                File.WriteAllText(path, File.ReadAllText(path).Replace(search, newvalue));
+                Console.WriteLine("Processed file '{0}'.", path);
             }
         }
     }
